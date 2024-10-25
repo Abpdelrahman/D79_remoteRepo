e4c56a9b159e8b18eee06e8a3411b4a415b83dc6
Abdelrahman  Ibrahim <abdibrahim@stu.okan.edu.tr>
2024-10-25 21:59:53 +0300

    version1

---
+++ File2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+printf("F2 sample ....Application 1");
+return 0;
+}
---
+++ File4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+printf("F1 sample ....Application 1");
+return 0;
+}
